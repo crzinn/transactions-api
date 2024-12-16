1f8bcb3314348112bb9e8614cbe7e80b6c7cf4e0
crzinn <crazydoidaao@gmail.com>
2024-12-16 17:14:59 -0300

    env and validations

---
+++ env/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { number, z } from 'zod'
+
+//process.env
+//criar um schema para as variaveis de ambiente (representação de uma estrutura de dados)
+const envSchema = z.object({
+    DATABASE_URL: z.string(),
+    PORT: z.number().default(3333),
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('production')
+})
+//analisa process.env com base em envSchema
+export const env = envSchema.parse(process.env)
+
+//estou validando se o process.env esta seguindo o envSchema, ou seja, DATABASE_URL e PORT devem seguir os formatos definidos em envSchema, se não um erro será retornado
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import fastify from "fastify"
 import { knex } from "./database"
-import { randomUUID } from "crypto"
+import { env } from "../env"
 
 const app = fastify()
 
 app.get("/", async (req, res) => {
-    const transaction = await knex('transactions').where('amount', 1000)
+    const transaction = await knex('transactions').where('amount', 1000).select('*')
 
     return transaction
 })
 
-app.listen({port: 3333}).then(() => {console.log('http server is running!')})
+app.listen({port: env.PORT}).then(() => {console.log('http server is running!')})
